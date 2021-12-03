a706fc3f31fa1e0aef6110ef1d1db0ef8639508b
davidh44 <70000000+davidh44@users.noreply.github.com>
2021-12-02 20:40:28 -0800

    Add files via upload

---
+++ scrapeGoogle.py
@@ -0,0 +1,106 @@
+import requests
+import urllib
+import sys
+import pandas as pd
+from requests_html import HTML
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
+
+def get_source(url):
+
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+        return response
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+
+def scrape_google(query):
+    query = urllib.parse.quote_plus(query)
+    response = get_source("https://www.google.com/search?q=" + query)
+    links = list(response.html.absolute_links)
+    # remove links to unwanted sites - can add any others
+    unwanted_domains = ('https://www.google.',
+                      'https://google.',
+                      'https://webcache.googleusercontent.',
+                      'http://webcache.googleusercontent.',
+                      'https://policies.google.',
+                      'https://support.google.',
+                      'https://maps.google.',
+                      'https://www.youtube.',
+                      'https://twitter.',
+                      'https://play.google.',
+                      'https://apps.apple.')
+    for url in links[:]:
+        if (url.startswith(unwanted_domains)):
+            links.remove(url)
+    # remove duplicate sites
+    for url1 in links[:]:
+        for url2 in links[:]:
+            if url1 == url2:
+                continue
+            if url2.startswith(url1):
+                links.remove(url2)
+    return links
+
+
+def scrape_sites(sites):
+    sites_words = []
+    for url in sites:
+        try:
+            output = scrape_site(url)
+            sites_words.append(output)
+        except:
+            print("Unable to scrape site:",url)
+    return sites_words
+
+
+def scrape_site(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    output = []
+    nontext = [
+        '\n',
+        '\n\n',
+        '\n\n\n',
+        '\n\n\n\n',
+        '\t',
+        '\t\t',
+        '',
+        ' ',
+        ',',
+        '.',
+        '-',
+        ';',
+        ':'
+    ]
+    for node in soup.findAll('p'):
+        for str in node.findAll(text=True):
+            words = str.split(' ')
+            for w in words:
+                if (w not in nontext):
+                    output.append(w)
+    return output
+
+def get_text_data(query):
+    sites = scrape_google(query)
+    scraped_text = scrape_sites(sites)
+    return scraped_text
+
+if __name__ == "__main__":
+    # change query here to user input
+    # query = sys.argv[1]
+    query = "text mining"
+    sites = scrape_google(query)
+    print(len(sites),'sites scraped for Google search of query:',query,'\n')
+    for s in sites:
+        print(s,"\n")
+    print("----------------------------")
+    print("Scraping sites........\n")
+    scraped_text = scrape_sites(sites)
+    print("----------------------------")
+    print('Scraped text.......\n')
+    print(scraped_text)
