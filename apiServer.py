a2a9e3dac9f8510c56360564895fd9121efddd4e
davidh44 <70000000+davidh44@users.noreply.github.com>
2021-12-08 23:27:18 -0800

    Update apiServer.py

--- apiServer.py
+++ apiServer.py
@@ -1,5 +1,5 @@
 from flask import Flask, make_response, jsonify, request, send_file
-# from ScrapeMineViz import get_visualizations
+from ScrapeMineViz import get_visualizations
 
 app = Flask(__name__)
 
